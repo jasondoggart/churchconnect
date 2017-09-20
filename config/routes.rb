fbecc0da30585fa207c76283754db0d376798cb9
Jason Doggart <jasondoggart@gmail.com>
2017-09-19 21:59:13 -0400

    updated user show page with dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users
   devise_scope :user do
     authenticated :user do
-      root :to => 'static_pages#home', as: :authenticated_root
+      root :to => 'users#show', as: :authenticated_root
     end
     unauthenticated :user do
       root :to => 'devise/registrations#new', as: :unauthenticated_root
