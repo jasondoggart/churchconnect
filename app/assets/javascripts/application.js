812828a9a9f29a75a60e87c5a6b32039c0705a44
Jason Doggart <jasondoggart@gmail.com>
2017-09-20 17:23:42 -0400

    added login page, fixed turbolinks error with page loading, started work on dashboard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,12 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+$(document).on('turbolinks:load', function() {
+  demo.checkFullPageBackgroundImage();
+
+  setTimeout(function() {
+      // after 1000 ms we add the class animated to the login/register card
+      $('.card').removeClass('card-hidden');
+  }, 700)
+});
+
