66b2c022346fb9752b0643b5820f1b5d488acc4c
Jason Doggart <jasondoggart@gmail.com>
2017-11-07 13:04:50 -0500

    paginate announcements

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -5,7 +5,7 @@ class AnnouncementsController < ApplicationController
   # GET /announcements
   # GET /announcements.json
   def index
-    @announcements = Announcement.all
+    @announcements = Announcement.paginate(:page => params[:page], :per_page => 6)
     @user = current_user
   end
 
